2b0755d3851c14f13dd547e31088255abfb77bc9
gaolei1201 <756985063@qq.com>
2018-06-17 10:24:13 +0800

    添加加载数据时，圆形进度条Dialog

--- MVPModel/app/src/main/java/com/gaolei/mvpmodel/rxjava/BaseObserver.java
+++ MVPModel/app/src/main/java/com/gaolei/mvpmodel/rxjava/BaseObserver.java
@@ -1,9 +1,11 @@
 package com.gaolei.mvpmodel.rxjava;
 
+import android.app.Dialog;
 import android.widget.Toast;
 
 import com.gaolei.mvpmodel.application.CustomApplication;
 import com.gaolei.mvpmodel.mview.BaseView;
+import com.gaolei.mvpmodel.view.CustomProgressDialog;
 
 import java.io.IOException;
 
@@ -17,23 +19,24 @@ public abstract class BaseObserver<T> extends ResourceObserver<T> {
     protected String errMsg = "";
     private BaseView mView;
     private boolean isShowError = true;
+
     protected BaseObserver(BaseView view){
         this.mView = view;
+//        Dialog mDialog = CustomProgressDialog.createLoadingDialog(this, "正在加载中...");
+//        mDialog.setCancelable(true);//允许返回
+//        mDialog.show();//显示
     }
     protected BaseObserver(BaseView view, boolean isShowError){
         this.mView = view;
         this.isShowError = isShowError;
     }
 
-//    @Override
-//    public void onSubscribe(Disposable d) {
-//        disposable = d;
-//    }
-//
-//    @Override
-//    public void onNext(T t) {
-//
-//    }
+
+
+    @Override
+    public void onNext(T t) {
+
+    }
 
     @Override
     public void onError(Throwable e) {
