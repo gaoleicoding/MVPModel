5d4076aaf4b496d3d61e8264200b224cffe84ad3
gaolei1201 <756985063@qq.com>
2018-06-29 00:12:05 +0800

    glide增加内存缓存大小设置

--- MVPModel/app/src/main/java/com/gaolei/mvpmodel/view/ImageLoader.java
+++ MVPModel/app/src/main/java/com/gaolei/mvpmodel/view/ImageLoader.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DecodeFormat;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
 
@@ -23,6 +24,7 @@ public class ImageLoader {
 //                .placeholder(R.drawable.ic_launcher)// 正在加载中的图片
 //                .error(R.drawable.video_error) // 加载失败的图片
                     .diskCacheStrategy(DiskCacheStrategy.ALL); // 磁盘缓存策略
+
         }
         return instance;
     }
--- MVPModel/app/src/main/java/com/gaolei/mvpmodel/view/MyAppGlideModule.java
+++ MVPModel/app/src/main/java/com/gaolei/mvpmodel/view/MyAppGlideModule.java
@@ -8,7 +8,10 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.load.DecodeFormat;
+import com.bumptech.glide.load.engine.bitmap_recycle.LruBitmapPool;
 import com.bumptech.glide.load.engine.cache.DiskLruCacheFactory;
+import com.bumptech.glide.load.engine.cache.LruResourceCache;
 import com.bumptech.glide.module.AppGlideModule;
 
 @GlideModule
@@ -25,6 +28,13 @@ public final class MyAppGlideModule extends AppGlideModule {
         builder.setDiskCache(
                 new DiskLruCacheFactory( getStorageDirectory(context)+"/GlideDisk", diskCacheSizeBytes )
         );
+        int maxMemory = (int) Runtime.getRuntime().maxMemory();//获取系统分配给应用的总内存大小
+        int memoryCacheSize = maxMemory / 8;//设置图片内存缓存占用八分之一
+        //设置内存缓存大小
+        builder.setMemoryCache(new LruResourceCache(memoryCacheSize));
+        //设置BitmapPool缓存内存大小
+        builder.setBitmapPool(new LruBitmapPool(memoryCacheSize));
+
     }
     private String getStorageDirectory(Context context){
         return Environment.getExternalStorageDirectory().getPath()+"/"+context.getPackageName();
